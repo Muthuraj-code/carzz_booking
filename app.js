0015d36ba1b276e6f45a06421af13753bcb766f6
Muthuraj <muthurajn37@gmail.com>
2020-02-25 22:00:18 +0530

    third commit

--- app.js
+++ app.js
@@ -9,7 +9,21 @@ app.use(express.static("public"));
 
 app.set('view engine','ejs');
 
-let drivers = ["Jhon","Lohith","Tony"];
+let drivers = [{
+  dName:"Jhon",
+  lan:"Kannada",
+  price:18
+},
+{
+  dName:"Lohith",
+  lan:"Hindi",
+  price:15
+},
+{
+  dName:"Tony",
+  lan:"English",
+  price:20
+}];
 let bookOption ={
   from_City:"",
   to_City:"",
@@ -20,33 +34,31 @@ let bookOption ={
   Address:"",
   eMail:""
 }
+let filtrDriver=[];
 
-app.get("/",function(req,res){
+app.get("/",(req,res)=>{
   res.render("index");
 })
-app.post("/",function(req,res){
+app.post("/",(req,res)=>{
 res.redirect("/");
 });
-app.get("/booking", function(req,res){
+app.get("/booking",(req,res)=>{
   // bookOption.from_City= res.body.frm;
   // bookOption.to_City=res.body.to;
   // console.log(bookOption);
 });
 
-app.post("/booking",function(req,res){
-  bookOption.from_City= req.body.frm;
-  bookOption.to_City=req.body.to;
-  bookOption.dateJ=req.body.dpt_date;
-  console.log(bookOption);
-  res.render("booking",{driver:drivers});
+app.post("/booking",(req,res)=>{
+
+  res.render("booking");
 });
 
-app.get("/confirm",function(req,res){
+app.get("/confirm",(req,res)=>{
 
 
 });
 
-app.post("/confirm",function(req,res){
+app.post("/confirm",(req,res)=>{
   bookOption.first_Name=req.body.fName;
   bookOption.last_Name=req.body.lName;
   bookOption.user_name=req.body.uName;
@@ -58,7 +70,27 @@ app.post("/confirm",function(req,res){
   let totalPrice=randomKM*15;
   res.render("confirm",{fName:bookOption.first_Name,lName:bookOption.last_Name,uName:bookOption.user_name,dOJ:bookOption.dateJ,add:bookOption.Address,tPrice:totalPrice,frm:bookOption.from_City,to:bookOption.to_City});
 });
-app.listen(process.env.PORT || 3000,function(){
+
+app.get("/driver",(req,res)=>{
+
+  res.render("driver",{drivers:filtrDriver});
+})
+app.post("/driver",(req,res)=>{
+  if(req.body.frm!=undefined){
+  bookOption.from_City= req.body.frm;
+  bookOption.to_City=req.body.to;
+  bookOption.dateJ=req.body.dpt_date;
+  console.log(bookOption);
+}
+  let langOption = req.body.lang;
+  console.log(langOption);
+  filtrDriver=drivers.filter((lan)=>{
+    return lan.lan===langOption;
+  })
+  // console.log(filtrDriver);
+res.redirect("/driver");
+})
+app.listen(process.env.PORT || 3000,()=>{
   console.log("Server started at port 3000");
 })
 
